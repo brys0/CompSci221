be13210711d33505ea95218120701572d14f252a
Bryson T. <35676267+brys0@users.noreply.github.com>
2026-04-18 09:43:49 -0500

    feat: check environment variables

--- seeding/index.ts
+++ seeding/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+import { sql, SQL } from "bun";
+
+const { MYSQL_DATABASE_HOST, MYSQL_DATABASE_USER, MYSQL_DATABSE_USER_PASSWORD } =
+  process.env;
+
+function validateEnvironment(): boolean {
+  let shouldExit = false;
+  if (!MYSQL_DATABASE_HOST) {
+    console.error('You must have a MYSQL_DATABASE_HOST defined.');
+    shouldExit = true;
+  }
+
+  if (!MYSQL_DATABASE_USER) {
+    console.error('You must have a MYSQL_DATABASE_USER defined.');
+    shouldExit = true;
+  }
+
+  if (!MYSQL_DATABSE_USER_PASSWORD) {
+    console.error('You must have a MYSQL_DATABASE_USER_PASSWORD defined.');
+    shouldExit = true;
+  }
+
+  return !shouldExit;
+}
+
+if (!validateEnvironment()) {
+  process.exit(1);
+}
+
+// const conn = new SQL();
